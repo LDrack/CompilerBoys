6c90add9fc9549b10ccb42929b52cc02053c229f
LDrack <39444912+LDrack@users.noreply.github.com>
2021-12-09 11:58:08 +0100

    Update VS Project Settings

--- MIECCompiler/MiniIEC/DACGenerator.cpp
+++ MIECCompiler/MiniIEC/DACGenerator.cpp
@@ -4,14 +4,17 @@ namespace MIEC {
 	DACGenerator::DACGenerator()
 	{
 	}
+
 	bool DACGenerator::AddStat(OpClass op, Operand* first)
 	{
 		return false;
 	}
+
 	bool DACGenerator::AddStat(OpClass op, Operand* first, Operand* second)
 	{
 		return false;
 	}
+
 	void DACGenerator::Print(std::wostream& wost)
 	{
 	}
